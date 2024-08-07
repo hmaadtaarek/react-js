48c3c7eca4141f6aef1b56a96975a0571a97b580
hmaadtarek <hammadjutthj6355@gmail.com>
2024-08-07 19:07:27 +0500

    tailwind project complete

--- Taikwindprops/src/App.jsx
+++ Taikwindprops/src/App.jsx
@@ -2,12 +2,64 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [color , setColor] = useState('black')
   return (
-    <>
+    <div className='w-screen h-screen duration-200 flex justify-center items-center selection:bg-white selection:text-black'
+    style={{ backgroundColor: color }}>
+    <h1 className='font-bold capitalize text-white'>"Background color changer"</h1>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-4'>
+        <div className="flex flex-wrap justify-center items-center gap-3 shadow-lg bg-white text-black px-3 py-2 rounded-3xl">
+
+
+        <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+        <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}
+          >Green</button>
+        <button
+          onClick={() => setColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
+        <button
+          onClick={() => setColor("olive")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "olive"}}
+          >Olive</button>
+        <button
+          onClick={() => setColor("grey")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "grey"}}
+          >Grey</button>
+        <button
+          onClick={() => setColor("yellow")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+          style={{backgroundColor: "yellow"}}
+          >Yellow</button>
+        <button
+          onClick={() => setColor("Pink")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+          style={{backgroundColor: "Pink"}}
+          >Pink</button>
+        <button
+          onClick={() => setColor("Purple")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+          style={{backgroundColor: "Purple"}}
+          >Purple</button>
+        <button
+          onClick={() => setColor("Lavender")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+          style={{backgroundColor: "Lavender"}}
+          >Lavender</button>
+        </div>
+      </div>
+    </div>
     
-      <h1 className='font-bold capitalize'>"Background color changer"</h1>
-    </>
   )
 }
 
