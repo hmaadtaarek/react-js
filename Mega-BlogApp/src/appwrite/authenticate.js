04627293ae6b06adf508e8fdf0d5b7d9b7a0b0bc
hmaadtarek <hammadjutthj6355@gmail.com>
2024-08-31 17:45:23 +0500

    gitignore

--- Mega-BlogApp/src/appwrite/authenticate.js
+++ Mega-BlogApp/src/appwrite/authenticate.js
@@ -5,4 +5,57 @@ export class AuthService {
     client = new Client()
     account
 
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                // will return login method
+                return this.login (email, password)
+            }
+            else {
+                return userAccount
+            }
+
+        }
+        catch (error) {
+            console.log("appwrite service :: createAccount :: error:", error)
+        }
+    }
+
+    async login({email, password}){
+        try {
+          await this.account.createEmailSession(email, password); 
+        } 
+        catch (error) {
+            console.log("appwrite service :: login :: error:", error)
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            await this.account.get()
+        } catch (error) {
+            console.log("appwrite service :: getCurrentUser :: error:", error)
+        }
+    }
+    
+    async logout() {
+        try {
+            await this.account.deleteSessions()
+        } 
+        catch (error) {
+            console.log("appwrite service :: logout :: error:", error)    
+        }
+    }
 }
+
+const authService = new AuthService()
+export default authService
